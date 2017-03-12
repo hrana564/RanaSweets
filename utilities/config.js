8dd013c19520c66b3a9c9fc54a0c1775cfb65814
hrana564 <hrana564@gmail.com>
2017-03-12 16:20:26 +0530

    Update config.js

--- utilities/config.js
+++ utilities/config.js
@@ -4,9 +4,9 @@
 module.exports = {
     AppName : "Rana Sweets",
     hostName : "http://localhost:8000/",
-    database : "mongodb://hrana564:Login123@ds141428.mlab.com:41428/ranasweets",
+    database : "mongodb://hrana564:<password>@ds141428.mlab.com:41428/ranasweets",
     process: process.env.PORT || 3000,
     secretKey: "Your Secret Key",
     mailFrom : "hprana15051994@gmail.com",
-    emailSetup: "smtps://hprana15051994%40gmail.com:Login123@smtp.gmail.com"
+    emailSetup: "smtps://hprana15051994%40gmail.com:<password>@smtp.gmail.com"
 }
